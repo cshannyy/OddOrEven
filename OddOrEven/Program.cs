c72c4876b607b3feac8503db21b4192f8c21c8cc
Colin Shanny <cshannyy@gmail.com>
2020-10-27 12:01:37 -0500

    created even or odd calc

--- OddOrEven/Program.cs
+++ OddOrEven/Program.cs
@@ -6,7 +6,27 @@ namespace OddOrEven
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine(IsOdd(6));
         }
+
+        public static bool IsOdd(int number)
+       {
+            Console.WriteLine("Enter number");
+            number = int.Parse(Console.ReadLine());
+
+            if (number % 2 == 0)
+            {
+                Console.WriteLine("Your number is Odd");
+
+                return true;
+            }
+            else
+            {
+                Console.WriteLine("Your number is even");
+
+                return false;
+            }
+
+       }
     }
 }
